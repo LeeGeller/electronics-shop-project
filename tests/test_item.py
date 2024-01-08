8759edc06e3bdb4be04af1a1a2b1a8885b595286
Kris <ssspers92@gmail.com>
2024-01-08 16:40:28 +0300

    Create test for object name

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,6 @@
-
 from src.item import Item
-def test_init_object_item(fixture_class):
-    assert fixture_class.name == "Смартфон"
+
+
+def test_init_object_item():
+    object_1 = Item("Смартфон", 10000, 20)
+    assert object_1.name == "Смартфон"
