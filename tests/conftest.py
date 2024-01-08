5d41014c1b619d2cbbddd040ad56b7938f0f5209
Kris <ssspers92@gmail.com>
2024-01-08 18:20:02 +0300

    reformate code

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from src.item import Item
 def fixture_class_item():
     return Item("Смартфон", 10000, 20)
 
+
 @pytest.fixture
 def fixture_class_item_2():
     return Item("No Смартфон", 100, 666)
