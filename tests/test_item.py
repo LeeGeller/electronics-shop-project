f749e9879dde020c3fe5912541a8b5f07be641ed
Kris <ssspers92@gmail.com>
2024-01-08 17:13:01 +0300

    Create tests for pay_rate and apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,9 +7,23 @@ def test_init_object_item():
     assert object_1.price == 10000
     assert object_1.quantity == 20
 
+
 def test_sum_total_price():
     object_1 = Item("Смартфон", 10000, 20)
     object_2 = Item("No Смартфон", 100, 666)
 
     assert object_1.calculate_total_price() == 10000 * 20
     assert object_2.calculate_total_price() == 100 * 666
+
+
+def test_pay_rate():
+    object_1 = Item("Смартфон", 10000, 20)
+    object_2 = Item("No Смартфон", 100, 666)
+
+    object_1.pay_rate = 0.5
+    object_1.apply_discount()
+
+    assert object_1.price == 10000 * 0.5
+    assert object_2.price == 100
+    assert object_1.calculate_total_price() == (10000 * 0.5) * 20
+    assert object_1.pay_rate == 0.5
