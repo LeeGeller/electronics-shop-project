4604c08a6be3de5005c095b151e3d5c457790691
Kris <ssspers92@gmail.com>
2024-01-08 17:25:57 +0300

    Append object in list all

--- src/item.py
+++ src/item.py
@@ -16,6 +16,7 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        self.all.append(Item)
 
     def calculate_total_price(self) -> float:
         """
