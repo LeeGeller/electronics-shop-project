0121b0385b1cbc0605b57afbb2d1c70df87c60ca
Kris <ssspers92@gmail.com>
2024-01-08 18:13:23 +0300

    Upgrade tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,43 +1,35 @@
 from src.item import Item
 
 
-def test_init_object_item():
-    object_1 = Item("Смартфон", 10000, 20)
-    assert object_1.name == "Смартфон"
-    assert object_1.price == 10000
-    assert object_1.quantity == 20
-
-
-def test_sum_total_price():
-    object_1 = Item("Смартфон", 10000, 20)
-    object_2 = Item("No Смартфон", 100, 666)
-
-    assert object_1.calculate_total_price() == 10000 * 20
-    assert object_2.calculate_total_price() == 100 * 666
-
-
-def test_pay_rate():
-    object_1 = Item("Смартфон", 10000, 20)
-    object_2 = Item("No Смартфон", 100, 666)
-
-    object_1.pay_rate = 0.5
-    object_1.apply_discount()
-
-    assert object_1.price == 10000 * 0.5
-    assert object_2.price == 100
-    assert object_1.calculate_total_price() == (10000 * 0.5) * 20
-    assert object_1.pay_rate == 0.5
-
-
-def test_item_all():
+def test_check_len_item_all_if_len_zero():
     assert len(Item.all) == 0
 
-    object_1 = Item("Смартфон", 10000, 20)
+
+def test_check_len_item_all_if_len_one(fixture_class_item):
     assert len(Item.all) == 1
 
-    object_2 = Item("No Смартфон", 100, 666)
-    assert len(Item.all) == 2
-    assert isinstance(Item.all, list)
 
+def test_init_object_item(fixture_class_item):
+    assert fixture_class_item.name == "Смартфон"
+    assert fixture_class_item.price == 10000
+    assert fixture_class_item.quantity == 20
+
+
+def test_sum_total_price(fixture_class_item, fixture_class_item_2):
+    assert fixture_class_item.calculate_total_price() == 10000 * 20
+    assert fixture_class_item_2.calculate_total_price() == 100 * 666
+
+
+def test_pay_rate(fixture_class_item, fixture_class_item_2):
+    fixture_class_item.pay_rate = 0.5
+    fixture_class_item.apply_discount()
+
+    assert fixture_class_item.price == 10000 * 0.5
+    assert fixture_class_item_2.price == 100
+    assert fixture_class_item.calculate_total_price() == (10000 * 0.5) * 20
+    assert fixture_class_item.pay_rate == 0.5
+
+
+def test_item_all(fixture_class_item, fixture_class_item_2):
     for value in Item.all:
         assert isinstance(value, object)
