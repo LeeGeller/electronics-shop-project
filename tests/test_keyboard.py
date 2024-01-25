5cafb17d73620283fb4bf42e64f5ed8cd287512d
Kris <ssspers92@gmail.com>
2024-01-25 20:43:11 +0300

    Created test_class

---
+++ tests/test_keyboard.py
@@ -0,0 +1,11 @@
+﻿from src.item import Item
+from src.keyboard import Keyboard, Mixin
+
+
+def test_class():
+    keyboard = Keyboard('Dark Project KD87A', 9600, 5)
+
+    assert isinstance(Keyboard, object)
+    assert issubclass(Keyboard, Item)
+    assert issubclass(Keyboard, Mixin)
+    assert str(keyboard) == "Dark Proje"
