ebc1f17d7a3bad8c17459844d5e4a57a68ce1eeb
Kris <ssspers92@gmail.com>
2024-02-01 00:14:35 +0300

    Upgrade

--- src/phone.py
+++ src/phone.py
@@ -7,7 +7,7 @@ class Phone(Item):
         self._number_of_sim = number_of_sim
 
     def __repr__(self):
-        return f"({Phone.__class__.__name__}'{self.name}', {self.price}, {self.quantity}, {self._number_of_sim})"
+        return f"{Phone.__name__}('{self.name}', {self.price}, {self.quantity}, {self._number_of_sim})"
 
     @property
     def number_of_sim(self):
